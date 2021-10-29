94e915ecee773ce38b0ace7ab18334b7fbc5573c
sytranvan2205 <sytranvan220501@gmail.com>
2021-10-29 13:59:24 +0700

    XYSs update frmLogin

---
+++ QuanLyLuongSanPham_GUI/frmLogin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyLuongSanPham_GUI
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+
+        private void txtUsername_Click(object sender, EventArgs e)
+        {
+            txtUsername.Clear();
+        }
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            txtPassword.Clear();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            txtPassword.PasswordChar = '*';
+        }
+    }
+}
---
+++ QuanLyLuongSanPham_GUI/frmXemThongTin.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace QuanLyLuongSanPham_GUI
+{
+    partial class frmXemThongTin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmXemThongTin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 246);
+            this.IconOptions.Image = global::QuanLyLuongSanPham_GUI.Properties.Resources.logo;
+            this.Name = "frmXemThongTin";
+            this.Text = "XemThongTin";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
