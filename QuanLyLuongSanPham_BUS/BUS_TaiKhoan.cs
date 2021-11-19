b602d18ed52c8019884d18da6080c85739333124
Dinh Huy <huydinhse@gmail.com>
2021-11-19 11:16:50 +0700

    Update frmLogin and fix some error basic

---
+++ QuanLyLuongSanPham_BUS/BUS_TaiKhoan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyLuongSanPham_DAO;
+using QuanLyLuongSanPham_DTO;
+
+namespace QuanLyLuongSanPham_BUS
+{
+    public class BUS_TaiKhoan
+    {
+        DAO_TaiKhoan taiKhoan;
+    }
+}
